ea2ed772bd9ab59544588d5e8911dab466da90b7
KasparM8 <kasparmeos8@gmail.com>
2024-04-13 10:41:00 +0300

    add week 7 files

---
+++ app.js
@@ -0,0 +1,40 @@
+const heroSpan = document.querySelector('#hero');
+const villainSpan = document.querySelector('#villain');
+const resultSpan = document.querySelector('#result');
+const launcherBtn =document.querySelector('button');
+const heroes = ['Harry Potter', 'Luke Skywalker', 'Batman','Cpt. America', 'Peter Griffin' ];
+const villains = ['Lord Voldemort', 'Darth Vader', 'Joker', 'Sauron', 'Thanos']
+const weapon = ['Lightsaber', 'banana peel', 'flip-flop', 'magic wand', 'M16']
+
+
+
+launcherBtn.addEventListener('click', () => {
+    const randomHeroIndex = Math.floor(Math.random() * heroes.length);
+    const randomHero = heroes[randomHeroIndex];
+    const randomHeroWeaponIndex = Math.floor(Math.random() * weapon.length)
+    const randomHeroWeapon = weapon[randomHeroWeaponIndex];
+    const randomVillainIndex = Math.floor(Math.random() * villains.length);
+    const randomVillain = villains[randomVillainIndex];
+    const randomVillainWeaponIndex = Math.floor(Math.random() * weapon.length)
+    const randomVillainWeapon = weapon[randomVillainWeaponIndex]
+    
+
+    heroSpan.innerHTML = `Hero ${randomHero} is armed with a ${randomHeroWeapon}.`;
+    villainSpan.innerHTML = `Villain ${randomVillain} is armed with a ${randomVillainWeapon}.`;
+
+    const heroStrike = randomHero.length + randomHeroWeapon.length;
+    const villainStrike = randomVillain.length + randomVillainWeapon.length;
+
+    let resultMessage = '';
+    
+    if(heroStrike === villainStrike) {
+        resultMessage= `It's a draw!`;
+    
+    } else if(heroStrike > villainStrike) {
+        resultMessage=`${randomHero} defeats ${randomVillain}, and saves the day!`;
+    } else {
+        resultMessage=`${randomVillain} defeats ${randomHero}. The Dark Side wins!`;
+    }
+    resultSpan.innerHTML = resultMessage;
+
+});
